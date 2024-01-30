89ec0d958ef1382c4d0fad0fe126a18840e4f6ce
luis f sousa <rumpertumpski@gmail.com>
2024-01-30 12:01:52 -0300

    update colors order

--- app/utils.ts
+++ app/utils.ts
@@ -3,13 +3,13 @@ export const formatCurrency = (value: number | string) =>
 
 export const getPayersColors = () =>
   [
-    'bg-red-400',
     'bg-blue-400',
-    'bg-yellow-400',
-    'bg-green-400',
-    'bg-orange-300',
-    'bg-teal-400',
+    'bg-red-400',
     'bg-violet-400',
+    'bg-amber-500',
+    'bg-green-400',
+    'bg-yellow-300',
+    'bg-teal-400',
     'bg-pink-300',
     'bg-cyan-400',
     'bg-slate-300'
